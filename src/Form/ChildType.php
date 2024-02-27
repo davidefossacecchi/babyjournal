7ca70bc15ef3d7b10079c1b0f3e8b775a5f639a4
davidefossacecchi <davide.fossacecchi@gmail.com>
2024-02-27 22:32:08 +0100

    feat: add the child form

---
+++ src/Form/ChildType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Child;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ChildType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, ['label' => 'Nome', 'required' => true])
+            ->add('birthDate', DateType::class, ['label' => 'Data di nascita', 'required' => true])
+            ->add('submit', SubmitType::class, ['label' => 'Aggiungi']);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(['data_class' => Child::class]);
+    }
+}
