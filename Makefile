58b22c3e45699c9849334ad013aaf172213e7883
davidefossacecchi <davide.fossacecchi@gmail.com>
2024-03-07 21:51:40 +0100

    chore: setups assetmapper, stimulus bundle and live components

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 export USER_ID := $(shell id -u)
 export GROUP_ID := $(shell id -g)
 
-.PHONY: up build down restart
+.PHONY: *
 up:
 	env USER_ID=$(USER_ID) GROUP_ID=$(GROUP_ID) docker-compose up -d
 build:
@@ -9,3 +9,6 @@ build:
 down:
 	env USER_ID=$(USER_ID) GROUP_ID=$(GROUP_ID) docker-compose down --remove-orphans
 restart: down up
+build-watch:
+	docker-compose exec php bin/console sass:build --watch
+
--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
 ];
